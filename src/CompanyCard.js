8fe4087ce429dc540d1ae56d5f62d7222ccf925b
rrghiker <chris.ryan.hart1@gmail.com>
2022-12-06 10:06:24 -0500

    added company logo functionality

--- src/CompanyCard.js
+++ src/CompanyCard.js
@@ -1,32 +1,16 @@
 import React from "react";
 import { Card, CardBody, CardText, CardTitle } from "reactstrap"
 import { useHistory } from "react-router-dom";
-import logo1 from './logos/logo1.png'
-import logo2 from './logos/logo2.png'
-import logo3 from './logos/logo3.png'
-import logo4 from './logos/logo4.png'
 
 
 function CompanyCard({handle, name,description,logoUrl}){
     const history = useHistory();
-    // const images = {'logo1': logo1, 'logo2': logo2, 'logo3': logo3, 'logo4':logo4}
-
-    // if(logoUrl !== null){
-    //     // 
-    //     let fileName = logoUrl.slice(5,11);
-    //     for (ele in images){
-    //         console.log(ele);
-    //     }
-    //     // loop through
-    // }
-
-    // const imageUrl = logoUrl === null ? [] : <img alt="sample" src={require(`.${logoUrl}`)}/>;
-
-    // console.log(logoUrl);
 
     const handleClick = () => { 
         history.push(`/companies/${handle}`);
     };
+
+    const hasLogo = logoUrl !== 'null' ? true : false;
 
     return (
         <Card
@@ -36,10 +20,10 @@ function CompanyCard({handle, name,description,logoUrl}){
             width: '18rem'
             }}
         >
-            <img
+            {hasLogo && <img
             alt="Sample"
-            src={`.${logoUrl}`}
-            />
+            src={logoUrl}
+            />}
             <CardBody>
                 <CardTitle tag="h5">
                     {name}
